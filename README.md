387bca8f1dbcf2fe141c3da59d13b83f79311298
Chad Keeven <chadkeeven@gmail.com>
2017-07-12 20:54:57 -0500

    second commit

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 # log_analysis
-Reads data on a linux vm. Uses `get_most_viewed(query)` and `get_days_with_bad_requests(query)` in *newsdatadb.py*.
+Reads data on a linux vm. Uses `run_query(query)` to fetch query results for `get_top3_viewed_articles()`, `get_most_popular_authors()` and `get_days_with_bad_requests()` in *newsdatadb.py*.
 
 # QuickStart
 
 ## Prerequisites
 -Python 2.7
 -VM of linux
+-import newsdata.sql into news database
 
 ### Running the Program
 1. Extract the folder *log_analysis.zip*
 2. Open up the file with vagrant
 3. Use Git Bash to run vagrant
 4. Open *newsdatadb.py* in a word editor
-5. Select a method in *newsdatadb.py* and enter a query.
+5. Select a method in *newsdatadb.py*
 6. Run command `python newsdatadb.py`
 
 ### Views 
 - all_requests = SELECT date_trunc('day'::text, log."time") AS day,count(*) AS num FROM log GROUP BY (date_trunc('day'::text, log."time"));
 
 - bad_requests =  SELECT date_trunc('day'::text, log."time") AS day,  count(*) AS num FROM log WHERE (log.status <> '200 OK'::text) GROUP BY (date_trunc('day'::text, log."time"));
+
 - bad_percent_v2 =  SELECT all_requests.day, round(((((bad_requests.num)::double precision / (all_requests.num)::double precision) * (100)::double precision))::numeric, 1) AS percent FROM all_requests, bad_requests WHERE (all_requests.day = bad_requests.day);
--- newsdatadb.py
+++ newsdatadb.py
@@ -7,10 +7,10 @@ DBNAME = "news"
 most_viewed_3articles_query = """select articles.title, count(path) as num
  from articles, log
  where log.status = '200 OK'
- and articles.slug = substring(log.path from articles.slug)
+ and log.path = concat('/article/', articles.slug)
  group by articles.title
  order by num desc limit 3;"""
-most_author_views_query = """"select authors.name, count(path) as num
+most_author_views_query = """select authors.name, count(path) as num
  from articles, log, authors
  where log.status = '200 OK'
  and articles.slug= substring(log.path from articles.slug)
@@ -24,27 +24,36 @@ bad_requests_query = """select cast(all_requests.day as date),
  and bad_percent_v2.percent > 1.0;"""
 
 
-def get_most_viewed(query):
+def run_query(query):
 
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     c.execute(query)
-    results = c.fetchall()
+    return c.fetchall()
+    db.close
+
+
+def get_top3_viewed_articles():
+
+    results = run_query(most_viewed_3articles_query)
+    print "The top three articles viewed:"
     for r in results:
         print str(r[0]) + " -- " + str(r[1]) + " views"
-    return results
-    db.close
 
 
-def get_days_with_bad_requests(query):
+def get_most_popular_authors():
 
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-    c.execute(query)
-    results = c.fetchall()
+    results = run_query(most_author_views_query)
+    print "The most popular authors:"
+    for r in results:
+        print str(r[0]) + " -- " + str(r[1]) + " views"
+
+
+def get_days_with_bad_requests():
+
+    results = run_query(bad_requests_query)
+    print "Days with more than 1% of bad requests:"
     for r in results:
         date = str(r[0])
-        formated_date = datetime.strptime(date, "%Y-%m-%d").strftime("%B %d,%Y")
-        print formated_date + " -- " + str(r[1]) + "% errors"
-    return results
-    db.close
+        fmt_date = datetime.strptime(date, "%Y-%m-%d").strftime("%B %d,%Y")
+        print fmt_date + " -- " + str(r[1]) + "% errors"
